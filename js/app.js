8857444d1be5c60aa7836450d82f8f0a968f0b9c
Denys Miasoiedov <grocerytrade@gmail.com>
2023-04-10 14:25:21 +0300

    added_all_info

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
   $(".header__link").click(function (event) {
-    $(".header__burger,.header__menu").toggleClass("active");
+    $(".header__burger,.header__menu,.header__link").toggleClass("active");
     $("body").toggleClass("lock");
   });
   // $(".kinesiology").click(function (event) {
