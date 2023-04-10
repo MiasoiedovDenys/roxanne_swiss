e97ef7290ca3dabdb4533432e3dee6cfa057939c
Denys Miasoiedov <grocerytrade@gmail.com>
2023-04-10 20:08:41 +0300

    added_maps

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger,.header__menu,.header__link").toggleClass("active");
-    $("body").toggleClass("lock");
+    $(".wrapper").toggleClass("lock");
   });
+
   $(".header__link").click(function (event) {
     $(".header__burger,.header__menu,.header__link").toggleClass("active");
-    $("body").toggleClass("lock");
+    $(".wrapper").toggleClass("lock");
   });
   // $(".kinesiology").click(function (event) {
   //   $(".page__section.drainage,").toggleClass("hidden");
